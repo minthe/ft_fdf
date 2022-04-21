934d56766219f99d9806303dee6b121424a4a52e
Valentin Fuhlenbrok <vfuhlenb@c2s3d6.42wolfsburg.de>
2022-04-21 19:19:46 +0200

    testing

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 18:38:02 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/04/21 16:53:07 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/04/21 19:17:51 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 # include <unistd.h>
 # include <mlx.h>
+# include <stdio.h>
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
 
 int	main(int argc, char *argv[]);
 
--- main.c
+++ main.c
@@ -6,20 +6,30 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 15:37:21 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/04/21 18:20:20 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/04/21 19:19:07 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+int	key_hook(int keycode, t_vars *vars)
+{
+	(void)vars;
+	(void)keycode;
+	printf("Hello from key_hook!\n");
+	return (0);
+}
+
 int	main(int argc, char *argv[])
 {
 	(void)argc;
 	(void)argv;
-	void	*mlx;
-	void	*win;
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 640, 360, "Tutorial Window");
-	mlx_loop(mlx);
+	t_vars	vars;
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.win, 640, 360, "Tutorial Window");
+
+	mlx_key_hook(vars.win, key_hook, &vars.win);
+
+	mlx_loop(vars.mlx);
 	return (0);
 }
