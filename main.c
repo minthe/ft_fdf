dcfa62e6b784e7d51a5cd4ecfa25f0529618c6a6
Valentin Fuhlenbrok <vfuhlenb@c2s3d6.42wolfsburg.de>
2022-05-11 20:16:12 +0200

    structure

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 15:37:21 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/05/11 20:04:16 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/05/11 20:15:54 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,16 @@ static void	print_lines(char *map)
 
 int	main(int argc, char *argv[])
 {
+	t_fdf	data;
+	int		*ptr[count_lines(data.map)]; // problem
+
 	if (argc != 2)
 		return (0);
-	t_fdf	data;
-
 	data.map = argv[1];
 	initialize_map(&data);
+	ptr[20] = data.win;
+	printf("ptr:%p\n", ptr[20]);
+	printf("ptr:%p\n", data.win);
 	printf("lines before: %d\n", data.lines); // debugging
 	fdf(&data);
 	print_lines(data.map); // debugging
