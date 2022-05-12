4b19395345db6ba71750a34d7f28c8f4a640ccad
Valentin Fuhlenbrok <vfuhlenb@c2s3d6.42wolfsburg.de>
2022-05-12 18:28:05 +0200

    map

--- functions/count_colums.c
+++ functions/count_colums.c
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 15:28:50 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/05/11 15:33:25 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/05/12 18:26:09 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,18 @@ int	count_colums(char *map)
 {
 	char	*line;
 	int		x;
+	int		i;
 	int		fd;
 
 	fd = open(map, O_RDONLY);
+	i = 0;
 	x = 0;
 	line = get_next_line(fd);
-	while (line > 0)
+	while (line[i] != '\n')
 	{
-		free(line);
-		line = get_next_line(fd);
-		x++;
+		if (line[i] == ' ')
+			x++;
+		i++;
 	}
 	free(line);
 	close(fd);
--- functions/initialize_map.c
+++ functions/initialize_map.c
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 18:37:51 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/05/12 17:08:06 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/05/12 18:27:03 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ void	initialize_map(t_fdf *data)
 {
 	data->mlx = mlx_init();
 	data->win = mlx_new_window(data->mlx, 640, 360, "FdF");
+	printf("colums: %d\n", count_colums(data->map));
 	data->lines = count_lines(data->map);
 	data->colums = count_colums(data->map);
 	data->height = (int **) malloc(sizeof(int) * data->lines);
 	if (data->height == NULL)
 		return ;
-	while ()
+
 }
