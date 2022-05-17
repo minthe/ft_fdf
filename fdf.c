ca2318976ba9c5d3b60bc03c5d3d9cd36eab174e
Valentin Fuhlenbrok <vfuhlenb@c2s3d6.42wolfsburg.de>
2022-05-17 18:38:45 +0200

    isometric view

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 12:05:23 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/05/17 18:13:23 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/05/17 18:36:12 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ void	fdf(t_fdf *data)
 	int scale_h;
 	int offset_x;
 
-	scale = 70;
-	offset_x = 350;
-	scale_h = 2;
+	scale = 10;
+	offset_x = 100;
+	scale_h = 1;
 	x = 0;
 	y = 0;
 	
@@ -36,10 +36,10 @@ void	fdf(t_fdf *data)
 			if (y < (data->lines - 1))
 			{
 				p1.x = x * scale + offset_x;
-				p1.y = y * scale - offset_x;
+				p1.y = y * scale - offset_x + 50;
 				p1.z = data->height[y][x] * scale_h;
 				p2.x = x * scale + offset_x;
-				p2.y = (y + 1) * scale - offset_x;
+				p2.y = (y + 1) * scale - offset_x + 50;
 				p2.z = data->height[y + 1][x] * scale_h;
 				isometric(&p1.x, &p1.y, p1.z);
 				isometric(&p2.x, &p2.y, p2.z);
@@ -48,10 +48,10 @@ void	fdf(t_fdf *data)
 			if (x < (data->colums - 1))
 			{
 				p1.x = x * scale + offset_x;
-				p1.y = y * scale - offset_x;
+				p1.y = y * scale - offset_x + 50;
 				p1.z = data->height[y][x] * scale_h;
 				p2.x = (x + 1) * scale + offset_x;
-				p2.y = y * scale - offset_x;
+				p2.y = y * scale - offset_x + 50;
 				p2.z = data->height[y][x + 1] * scale_h;
 				isometric(&p1.x, &p1.y, p1.z);
 				isometric(&p2.x, &p2.y, p2.z);
@@ -62,6 +62,5 @@ void	fdf(t_fdf *data)
 		x = 0;
 		y++;
 	}
-	my_mlx_pixel_put(data, 5, 5, 0x00FF0000);
 	mlx_put_image_to_window(data->mlx, data->win, data->image->img, 0, 0);
 }
