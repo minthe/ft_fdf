111b6081b6f360078412a1b1293e3ab83e56ac92
Valentin Fuhlenbrok <vfuhlenb@c2s19d125.42wolfsburg.de>
2022-05-22 13:36:49 +0200

    fixed edge case

--- functions/fills_grid.c
+++ functions/fills_grid.c
@@ -6,11 +6,34 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 17:26:32 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/05/22 12:22:07 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/05/22 13:30:33 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
+
+static void	check_validity(t_fdf *data, char **values)
+{
+	int	x;
+	int	i;
+
+	x = 0;
+	i = 0;
+	while (x < data->colums)
+	{
+		while (values[x][i])
+		{
+			if ((ft_isdigit(values[x][i]) == 0) && values[x][i] != '\n')
+			{
+				free_array(values);
+				error_msg(data, "-- Invalid Map --\n");
+			}
+			i++;
+		}
+		i = 0;
+		x++;
+	}
+}
 
 static void	handle_values(t_fdf *data, char **values, int *y)
 {
@@ -19,8 +42,7 @@ static void	handle_values(t_fdf *data, char **values, int *y)
 	x = 0;
 	while (x < data->colums)
 	{
-		if (!values[x] || (x == data->colums - 1 && values[x + 1]) \
-			|| !ft_atoi(values[x]))
+		if (!values[x] || (x == data->colums - 1 && values[x + 1]))
 		{
 			free_array(values);
 			error_msg(data, "-- Invalid Map --\n");
@@ -50,6 +72,7 @@ void	fills_grid(t_fdf *data)
 		data->height[y] = (int *) ft_calloc(sizeof(int), data->colums);
 		values = ft_split(line, ' ');
 		free(line);
+		check_validity(data, values);
 		handle_values(data, values, &y);
 		line = get_next_line(fd);
 		y++;
